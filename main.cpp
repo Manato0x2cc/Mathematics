391fcab015eb61e153c9f624d225cff9af1d0f25
Manato <manato0x2cc@gmail.com>
2017-07-03 17:09:02 +0900

    これなんやで。素数の時の出力すると完全数と友愛数隠れちゃうよ

---
+++ main.cpp
@@ -0,0 +1,52 @@
+//
+//  main.cpp
+//  Amicable Numbers
+//
+//  Created by Manato0x2cc on 2017/07/03.
+//  Copyright © 2017年 Manato0x2cc. All rights reserved.
+//
+
+#include <iostream>
+
+unsigned long getSumOfDivisors(unsigned long l);
+
+
+unsigned long getSumOfDivisors(unsigned long l){
+    unsigned long result = 1;
+    unsigned long lastDiv = 1;
+    unsigned long i = 0;
+    while ((lastDiv != l / (lastDiv + i)) && lastDiv + i < l){
+        ++i;
+        if(l % (lastDiv + i) == 0){
+            lastDiv += i;
+            result += l / lastDiv;
+            result += lastDiv;
+            i = 0;
+        }
+    }
+    
+    return result;
+}
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Searching Amicable Numbers\n";
+    
+    unsigned long i = 0;
+    unsigned long f = 2;
+    unsigned long pair = 0;
+    
+    for (unsigned long i = 0; i < ULONG_MAX - 2; ++i) {
+        if(getSumOfDivisors(f) == 1) {
+            std::cout << std::to_string(f) + "...こいつは素数やなぁ!" << std::endl;
+        }
+        if(f == (getSumOfDivisors(pair = getSumOfDivisors(f)))){
+            if(f == pair){
+                std::cout << std::to_string(f) + "...こいつは完全数やなぁ!" << std::endl;
+            }else{
+                std::cout << std::to_string(f) + "," + std::to_string(pair) + "...こいつは友愛数やなぁ!"<< std::endl;
+            }
+        }
+        f ++;
+    }
+    return 0;
+}
